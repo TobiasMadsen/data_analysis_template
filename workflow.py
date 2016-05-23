eb098b423fcda98d1c9f2ff1eef297e2e17ad627
TobiasMadsen <tobias.madsen@gmail.com>
2016-05-23 09:12:08 +0200

    Example analysis

--- workflow.py
+++ workflow.py
@@ -9,3 +9,10 @@ target("Download_Data",
 ./scripts/getRawData.sh
 '''
 
+# Do an analysis
+target("Example_Analysis",
+       input="data/housing.data",
+       output=["analysis/some_analysis/some_analysis.html",
+               "figures/price_age.png"]) << '''
+(cd analysis/some_analysis/ && Rscript -e 'library(knitr); rmarkdown::render("some_analysis.Rmd")' )
+'''
